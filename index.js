d71ca37a464c73077aa4dcefabd3304ea073e20c
Faridun Kudusov <FaridunK@itstep.uz>
2024-10-09 16:09:09 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let count = 0;
+const counter = document.getElementById('counter');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+
+incrementButton.addEventListener('click', () => {
+    count++;
+    counter.textContent = count;
+});
+
+decrementButton.addEventListener('click', () => {
+    count--;
+    counter.textContent = count;
+});
